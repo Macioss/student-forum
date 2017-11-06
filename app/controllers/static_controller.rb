ed639e639c34d166d3b462bec3e296e26fffa96a
Macioss <maciejko94@gmail.com>
2017-11-06 16:20:43 +0100

    Lab3

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,8 @@
 class StaticController < ApplicationController
   def index
-  @students = Student.all
-  @courses = Course.all
+    @students = Student.all
+    @courses = Course.all
+    @topics = Topic.all
+    @posts = Post.all
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,6 @@ class Student < ApplicationRecord
   has_many :posts
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: true
   validates :index, presence: true, length: { minimum: 6, maximum: 6 }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
